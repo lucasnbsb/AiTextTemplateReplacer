38d4dba6ead3539147e6a07914b86be0c8ae3828
lucas.neto <lucas.neto@bairesdev.com>
2023-03-15 13:01:56 -0300

    Initial commit

---
+++ src/app/templates.ts
@@ -0,0 +1,56 @@
+export const templates = {
+  formal: `
+    Dear [Recipient’s Name],
+  
+    I hope this email finds you well. I wanted to introduce you to [Company Name], 
+    a business that I believe could be of interest to you.
+    
+    [Company Name] is a [brief description of the company's products/services and any notable achievements or awards]. 
+    They have been operating in the industry for [number of years] and have established a strong reputation for [company's strengths].
+    
+    I believe there may be a great business opportunity for you and [Company Name] to collaborate, given your [common interests/goals]. 
+    I think their services/products could greatly benefit your business and I would be happy to facilitate an introduction between you and [Company Name] if you are interested.
+    
+    Please let me know if you would like to learn more about [Company Name] and explore the potential business opportunity. 
+    I can arrange a call between you and [Company Name] to discuss further.
+    
+    Thank you for your time and consideration. I look forward to hearing back from you.
+    
+    Best regards,
+    
+    [Your Name]
+  `,
+
+  formal2: `Dear [Recipient’s Name],
+
+  I hope this message finds you well. I am writing to introduce you to a noteworthy company that may be of interest to you. 
+  The company is called [Company Name] and they have been operating in the [Industry] for [Years]. 
+  They are recognized for their [Company Strengths].
+  
+  Given your [common interests/goals], I believe there may be a great opportunity for you and [Company Name] to collaborate. 
+  Their services/products could offer significant value to your business.
+  
+  If you would like to learn more about [Company Name] and explore the potential for collaboration, 
+  I would be delighted to facilitate an introduction between you and their team. We can arrange a call for you both to discuss further.
+  
+  Thank you for considering this opportunity. Please let me know if you are interested, and we can take it from there.
+  
+  Best regards,`,
+
+  very_informal: `Hey [Recipient’s Name],
+
+  How's it going? I wanted to introduce you to this cool company I know called [Company Name]. 
+  They've been around for a while and are doing some pretty cool stuff in [describe the industry or niche the company is in].
+  
+  I think there could be a sweet business opportunity between you and [Company Name], 
+  especially because you guys are both into [common interests/goals]. I believe their services/products could really help your business out.
+  
+  Would you be interested in learning more about [Company Name] and possibly collaborating with them? 
+  If you are, I can set up a call between you and them to chat further.
+  
+  Let me know what you think and we can go from there.
+  
+  Cheers,
+  
+  [Your Name]`,
+};
